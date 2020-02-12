e3a38ff42ad0e7c221ae09ef89ff48aeb0627d8f
Mark <markoreilly1992@gmail.com>
2020-02-12 22:01:54 +0000

    added sqlalchemy model for lego to pipeline

--- app/scraper/scraper/pipelines.py
+++ app/scraper/scraper/pipelines.py
@@ -1,7 +1,33 @@
 import os
-from app.app.models import Lego
 from sqlalchemy.orm import sessionmaker
-basedir = os.path.abspath(os.path.dirname(__file__))
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, String, Text, Integer
+
+
+basedir = 'C:\\Users\\marko\\OneDrive\\DOCUME~1\\09-LEG~1\\'
+DeclarativeBase = declarative_base()
+
+
+class Lego(DeclarativeBase):
+    __tablename__='lego'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(255))
+    name = Column(String(255))
+    price = Column(String(255))
+    description = Column(Text())
+    rating = Column(String(255))
+    available = Column(String(255))
+
+    def __init__(
+            self, model=None, name=None, price=None,
+            description=None, rating=None, available=None
+        ):
+        self.model = model
+        self.name = name
+        self.price = price
+        self.description = description
+        self.rating = rating
+        self.available = available
 
 
 class LegoPipeline(object):
@@ -11,7 +37,7 @@ class LegoPipeline(object):
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
         self.engine = create_engine(SQLALCHEMY_DATABASE_URI)
-        self.conn = engine.connect()
+        self.conn = self.engine.connect()
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
 
