0ef354578214cd94fa99b6c980a99e3b9c62e861
Mark <markoreilly1992@gmail.com>
2020-02-12 21:36:46 +0000

    added sqlalchemy and migrate to init

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import config
 
-
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
+
+from app import models
