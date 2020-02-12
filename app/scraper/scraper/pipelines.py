9c379cd5dc7232d960ee4cfc539e0b35f0fbc1b3
Mark <markoreilly1992@gmail.com>
2020-02-12 21:36:59 +0000

    added sqlalchemy to pipeline

--- app/scraper/scraper/pipelines.py
+++ app/scraper/scraper/pipelines.py
@@ -1,11 +1,29 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+from app.app.models import Lego
+from sqlalchemy.orm import sessionmaker
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class LegoPipeline(object):
+
+
+    def __init__(self):
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+        self.engine = create_engine(SQLALCHEMY_DATABASE_URI)
+        self.conn = engine.connect()
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+
+
     def process_item(self, item, spider):
-        return item
+        lego = Lego(
+            model=item['model'],
+            name=item['name'],
+            price=item['price'],
+            description=item['description'],
+            rating=item['rating'],
+            available=item['available']
+        )
+        self.session.add(lego)
+        self.session.commit()
