bdf8c399d9678fbdcdf6cec6ff2f5d1a4e328e95
Mark <markoreilly1992@gmail.com>
2020-02-12 20:47:27 +0000

    added link to pipeline in settings.py

--- app/scraper/scraper/settings.py
+++ app/scraper/scraper/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scraper.pipelines.ScraperPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'scraper.pipelines.LegoPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
