99f89a0698826f5a95e5e6bd3cc5f6291322a173
Mark <markoreilly1992@gmail.com>
2020-02-12 20:46:55 +0000

    added LegoItem

--- app/scraper/scraper/items.py
+++ app/scraper/scraper/items.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class ScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class LegoItem(scrapy.Item):
+    model = scrapy.Field()
+    name = scrapy.Field()
+    price = scrapy.Field()
+    description = scrapy.Field()
+    rating = scrapy.Field()
+    available = scrapy.Field()
     pass
